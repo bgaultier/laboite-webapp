52de647011e19a6204ba42742d105a44c7b567aa
Baptiste Gaultier <b.gaultier@gmail.com>
2013-07-19 19:13:14 +0200

    SQL dump modified

--- laboite-webapp.sql
+++ laboite-webapp.sql
@@ -1,6 +1,5 @@
 -- laboite-webapp SQL Dump
 -- version 0.1
--- http://www.phpmyadmin.net
 -- 
 
 -- --------------------------------------------------------
@@ -23,7 +22,20 @@ CREATE TABLE IF NOT EXISTS apps (
   direction_needed tinyint(1) NOT NULL DEFAULT '0',
   station_needed tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (id)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+-- 
+-- Contenu de la table `apps`
+-- 
+
+INSERT INTO apps VALUES (1, 'Time', 'Shows the clock time', 1, 0, 0, 0, 0, 0, 0, 0, 0);
+INSERT INTO apps VALUES (2, 'Weather', 'Displays the weather forecast based on data received from wunderground.com', 1, 0, 0, 0, 0, 0, 0, 0, 0);
+INSERT INTO apps VALUES (3, 'Buses', 'Displays the number of minutes you have to wait for the next bus at your stop (data.keolis-rennes.com)', 0, 0, 0, 0, 0, 1, 1, 1, 0);
+INSERT INTO apps VALUES (4, 'Bikes', 'Displays the number of bikes available at your bike station (data.keolis-rennes.com)', 0, 0, 0, 0, 0, 0, 0, 0, 1);
+INSERT INTO apps VALUES (5, 'Energy', 'Displays the energy consumption history chart of an emoncms.org feed', 0, 0, 0, 1, 0, 0, 0, 0, 0);
+INSERT INTO apps VALUES (6, 'Waves', 'Displays the predicted quality and conditions of a surf spot', 1, 0, 0, 0, 0, 0, 0, 0, 0);
+INSERT INTO apps VALUES (7, 'Messages', 'Displays messages sent by your friends', 0, 0, 0, 0, 0, 0, 0, 0, 0);
+INSERT INTO apps VALUES (8, 'Coffees', 'Displays your daily coffee consumption', 0, 0, 0, 1, 0, 0, 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -47,9 +59,9 @@ CREATE TABLE IF NOT EXISTS device_apps (
 CREATE TABLE IF NOT EXISTS devices (
   id int(11) NOT NULL AUTO_INCREMENT,
   creator int(11) NOT NULL,
-  `name` varchar(32) CHARACTER SET utf8 NOT NULL,
-  location varchar(128) CHARACTER SET utf8 NOT NULL,
-  apikey varchar(8) CHARACTER SET utf8 NOT NULL,
+  `name` varchar(32) NOT NULL,
+  location varchar(128) NOT NULL,
+  apikey varchar(8) NOT NULL,
   lastactivity timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (id)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
