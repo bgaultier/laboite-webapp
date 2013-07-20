530ee03c5074be1a6079040a30b48687ec315e4b
Baptiste Gaultier <b.gaultier@gmail.com>
2013-07-20 11:30:02 +0200

    signup link added

--- templates/login.php
+++ templates/login.php
@@ -19,6 +19,9 @@
       <h2 class="form-signin-heading"><?php echo _('Connexion'); ?></h2>
       <input type="text" name="email" class="input-block-level" placeholder="<?php echo _('Adresse e-mail'); ?>">
       <input type="password" name="password" class="input-block-level" placeholder="<?php echo _('Mot de passe'); ?>">
+      <div style="text-align:center; margin-bottom:16px;">
+        <a href="signup"><?php echo _('Créer un compte ?'); ?></a> - <a href="signup"><?php echo _('Mot de passe oublié ?'); ?>
+      </div>
       <button class="btn btn-inverse" type="submit"><?php echo _('Connexion'); ?></button>
     </form>
   </div> <!-- /container -->
