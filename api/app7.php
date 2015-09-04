0288acb226a8c30476cfbb97a2e88a1d990b1345
Baptiste Gaultier <b.gaultier@gmail.com>
2015-09-04 12:32:20 +0200

    Message app comma bug fixed

--- api/app7.php
+++ api/app7.php
@@ -1,4 +1,4 @@
 <?php
   $device = get_device_by_apikey($_GET['apikey']);
-  $data["messages"] = stripslashes(get_device_last_message($device['id']));
+  $data["messages"] = stripslashes(str_replace (',', '',get_device_last_message($device['id'])));
 ?>
