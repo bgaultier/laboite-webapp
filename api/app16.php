8bfa22e124b2be24f8af362a18c64cc38a9f0cc1
Baptiste Gaultier <b.gaultier@gmail.com>
2015-12-15 11:34:42 +0100

    app16.php became an example app explained in help

--- api/app16.php
+++ api/app16.php
@@ -1,11 +1,25 @@
 <?php
-	$station1 ="Insa";
+	// URL to get information about a bike station (STAR API)
+	$url = "https://data.explore.star.fr/api/records/1.0/search/?dataset=vls-stations-etat-tr&sort=idstation&facet=nom&facet=etat&facet=nombreemplacementsactuels&facet=nombreemplacementsdisponibles&facet=nombrevelosdisponibles&apikey=d55230a97e137bd4b073009462489f85d6486c1c242fb43db3d152a7&refine.nom=";
+	// Here is the name of our station
+	$name = "Place+de+Bretagne";
+	$url .= $name;
 
-	$json_string = file_get_contents("http://m.starbusmetro.fr/?q=rennes_gtfs_poisuggestions/$station1");
+	// GET the json file coming from STAR API
+	$json_string = file_get_contents($url);
 
+	// Parse the JSON
 	$parsed_json = json_decode($json_string);
 
-	var_dump($parsed_json);
+	// Path of the ressource
+	$records = $parsed_json->{'records'}[0]->{'fields'};
 
-	$stops = array();
+	// Extract the information
+	$bikes_available = $records->{'nombrevelosdisponibles'};
+
+	if(isset($bikes_available))
+		$data["$bikes_available"] = (int)$bikes_available;
+
+	// echo the info
+	echo "Rides available at Place de Bretagne station : $bikes_available\n";
 ?>
